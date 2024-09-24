77f9489e15d481cad730f1eca630f6e96951fd6f
luizengdev <luizengdev@gmail.com>
2024-09-24 18:19:46 -0300

    feat: add PhoneItem component and Footer to layout

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PhoneItem from "@/app/_components/phone-item"
 import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
@@ -89,8 +90,7 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       {/* CONTATO */}
       <div className="space-y-3 p-5">
         {barbershop.phones.map((phone) => (
-          // <PhoneItem key={phone} phone={phone} />
-          <p key={phone}>{phone}</p>
+          <PhoneItem key={phone} phone={phone} />
         ))}
       </div>
     </div>
